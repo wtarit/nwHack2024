7796d9d50e02d929bb0adf5e82132fdec87a73e5
Elhan Iqbal <e31iqbal@gmail.com>
2024-01-20 12:37:09 -0800

    DB Connected

--- server/app.js
+++ server/app.js
@@ -1,18 +1,22 @@
 const express = require('express');
 const mongoose = require('mongoose')
 
-// mongoose.connect('mongodb://localhost:27017/', {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true,
-// });
 
-// const db = mongoose.connection;
+const dbURL = 'mongodb+srv://wtarit:PsdByICI2OqOq25P@cluster0.akdqfhs.mongodb.net/?retryWrites=true&w=majority'
 
-// db.on("error", console.error.bind(console, "connection error: "));
 
-// db.once("open", ()=> {
-//     console.log("Database connection successful!");
-// });
+mongoose.connect(dbURL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+
+db.on("error", console.error.bind(console, "connection error: "));
+
+db.once("open", ()=> {
+    console.log("Database connection successful!");
+});
 
 const app = express();
 
