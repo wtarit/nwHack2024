07a2d292ddc5360b3532e432b0d14b7df22a13d1
Elhan Iqbal <e31iqbal@gmail.com>
2024-01-20 12:26:53 -0800

    server set up

--- server/app.js
+++ server/app.js
@@ -1,17 +1,23 @@
-const app = require('express');
-const router = app.router()
+const express = require('express');
+const mongoose = require('mongoose')
 
-mongoose.connect('mongodb://localhost:27017/', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-});
+// mongoose.connect('mongodb://localhost:27017/', {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+// });
 
-const db = mongoose.connection;
+// const db = mongoose.connection;
 
-db.on("error", console.error.bind(console, "connection error: "));
+// db.on("error", console.error.bind(console, "connection error: "));
 
-db.once("open", ()=> {
-    console.log("Database connection successful!");
-});
+// db.once("open", ()=> {
+//     console.log("Database connection successful!");
+// });
 
 const app = express();
+
+
+app.listen(3000, () => {
+    console.log('Server open on port 3000');
+
+});
