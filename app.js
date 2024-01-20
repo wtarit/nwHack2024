2deaf2f516229d9842568e9544d1456ce5f04fd3
Elhan Iqbal <e31iqbal@gmail.com>
2024-01-20 14:34:26 -0800

    Auth0 in the works

--- app.js
+++ app.js
@@ -1,6 +1,20 @@
 const express = require('express');
 const mongoose = require('mongoose');
-var path = require('path');
+var path = require('path'); 
+app.use(express.json());
+app.use(express.urlencoded({extended:true}))
+const { auth } = require('express-openid-connect');
+require('dotenv').config();
+
+const config = {
+    authRequired: false,
+    auth0Logout: true,
+    secret: process.env.SECRET,
+    baseURL: process.env.BASEURL,
+    clientID: process.env.CLIENTID,
+    issuerBaseURL: process.env.ISSUERBASEURL
+  }; 
+
 
 
 const dbURL = 'mongodb+srv://wtarit:PsdByICI2OqOq25P@cluster0.akdqfhs.mongodb.net/?retryWrites=true&w=majority'
@@ -25,7 +39,8 @@ const app = express();
 app.set('view engine', 'html');
 app.engine('html', require('ejs').renderFile);
 app.use(express.static(path.join(__dirname, '\/views')));
-
+// auth router attaches /login, /logout, and /callback routes to the baseURL
+app.use(auth(config));
 
 app.get("/", (req,res) => {
     res.render('home.html')
