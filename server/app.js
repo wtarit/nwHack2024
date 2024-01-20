c9e3e5de041aa8178a35801f2bd5f4dabeed849b
Elhan Iqbal <e31iqbal@gmail.com>
2024-01-20 12:22:10 -0800

    Require DB Connection Mongo Atlas

---
+++ server/app.js
@@ -0,0 +1,17 @@
+const app = require('express');
+const router = app.router()
+
+mongoose.connect('mongodb://localhost:27017/', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+
+db.on("error", console.error.bind(console, "connection error: "));
+
+db.once("open", ()=> {
+    console.log("Database connection successful!");
+});
+
+const app = express();
